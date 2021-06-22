983a454f4a8f10d6acd5ea45520ecb827a2b863c
Search-Pankaj <85724309+Search-Pankaj@users.noreply.github.com>
2021-06-22 11:46:06 +0530

    Time: 76 ms (69.72%), Space: 19.8 MB (96.62%) - LeetHub

---
+++ 3sum/3sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+ vector<vector<int>> threeSum(vector<int>& nums) {
+ 
+    vector<vector<int>> triples;
+    sort(nums.begin(), nums.end());
+    int i = 0, last = nums.size() - 1;
+    while (i < last) {
+        int a = nums[i], j = i+1, k = last;
+        while (j < k) {
+            int b = nums[j], c = nums[k], sum = a+b+c;
+            if (sum == 0) triples.push_back({a, b, c});
+            if (sum <= 0) while (nums[j] == b && j < k) j++;
+            if (sum >= 0) while (nums[k] == c && j < k) k--;
+        }
+        while (nums[i] == a && i < last) i++;
+    }
+    return triples;
+ }
+};
