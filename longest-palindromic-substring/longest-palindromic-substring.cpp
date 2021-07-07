a6f53f447727c0137516385b50e10b4a18e2e53a
Search-Pankaj <85724309+Search-Pankaj@users.noreply.github.com>
2021-07-07 18:16:43 +0530

    Time: 152 ms (49.53%), Space: 25.7 MB (43.21%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
-    string expand(string A ,int l,int r)
- {      int n = A.length() ;
-        while(l>= 0 and r< n and  A[l] == A[r] ){
-            l--;
-            r++ ;
-        }
-        return A.substr(l+1,r-l-1);
-}
-    string longestPalindrome(string A) {
-        int n = A.length();
-if (n == 0) return "";
+       string longestPalindrome(string s)
+    {
+        int n = s.size();
+        if (n == 0)
+            return "";
 
-string longest = A.substr(0,1) ;
-for(int i = 0 ; i < n-1 ;i++){
-    string p1 = expand(A,i,i);
-    if(p1.length() > longest.length()){
-        longest = p1;
-    }
-    string p2 = expand(A,i,i+1) ;
-    if(p2.length() > longest.length() ){
-        longest = p2;
-    }
-}
-    return longest ;
+        // dp[i][j] will be 'true' if the string from index i to j is a palindrome.
+        bool dp[n][n];
+
+        //Initialize with false
+
+        memset(dp, 0, sizeof(dp));
+
+        //Every Single character is palindrome
+        for (int i = 0; i < n; i++)
+            dp[i][i] = true;
+
+        string ans = "";
+        ans += s[0];
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (s[i] == s[j])
+                {
+                    //If it is of two character OR if its susbtring is palindrome.
+                    if (j - i == 1 || dp[i + 1][j - 1])
+                    {
+                        //Then it will also a palindrome substring
+                        dp[i][j] = true;
+
+                        //Check for Longest Palindrome substring
+                        if (ans.size() < j - i + 1)
+                            ans = s.substr(i, j - i + 1);
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
