17707fbcca9306754be3a50459e7a0f0d10d711f
Search-Pankaj <85724309+Search-Pankaj@users.noreply.github.com>
2021-07-22 19:29:31 +0530

    Time: 16 ms (55.44%), Space: 12.3 MB (6.67%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& num, int target) {
-            map<int, int> M;
-            vector<int> ans;
-
-            for (int i = 0; i < num.size(); i++) {
-                int required = target - num[i];
-                if (M.find(required) != M.end()) {
-                    // We have our solution. index1 = M[required], index2 = i + 1 ( 1 based ). 
-                    ans.push_back(M[required]);
-                    ans.push_back(i );
-                    return ans;
-                }
-                if (M.find(num[i]) == M.end()) {
-                    // We want to maintain the minimum index at M[num[i]]. i + 1 will 
-                    // obviously be greater than whats already present.
-                    M[num[i]] = i ;
-                }
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> vec;
+        map<int,int> set ;
+        int size = nums.size() - 1 ;
+      
+        for(int i = 0 ;i<= size  ; i++){
+            if(set.count(target - nums[i]) ){
+                vec.push_back(set[target - nums[i]]) ;
+                vec.push_back( i) ;
             }
-            return ans;
-}
+            
+            else{
+               set[nums[i]] = i ;  
+            }
+               
+            
+        }
+        return vec ;
+    }
 };
